398dda3ac73dcd220b36d36ee43582dce667d94c
Arnas Bendaravicius <arnasbendaravicius@gmail.com>
2025-08-07 14:18:06 +0300

    add & configure alpinejs, tailwindcss

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import "./bootstrap";
+
+document.addEventListener("alpine:init", () => {
+    Alpine.data("planSelection", () => ({
+        plans: [],
+
+        init() {
+            this.plans = window.plansData || [];
+        },
+    }));
+});
+
+Alpine.start();
